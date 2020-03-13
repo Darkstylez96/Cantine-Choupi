43e95d3499959e537d50258e7da5bd38a303582f
Darkstylez96 <60876044+Darkstylez96@users.noreply.github.com>
2020-03-13 16:57:41 +0100

    Add files via upload

--- src/components/Products.js
+++ src/components/Products.js
@@ -7,7 +7,7 @@ export default class Products extends Component {
             <div className="col-md-4" key={product.id}>
                 <div className="thumbnail text-center">
                     <a href={`#${product.id}`}onClick={(e)=>this.props.handleAddToCart(e, product)}>
-                        <img src={`products/_.png`} alt={product.title} />
+                        <img src={`products`} alt={product.title} />
                         <p>{product.title}</p>                        
                     </a>
                     <b>{util.formatCurrency(product.price)}</b>
